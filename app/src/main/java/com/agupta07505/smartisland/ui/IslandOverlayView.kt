3159024c4d501b3814687b2bcf1172349bc3acfa
agupta.07505 <agupta.07505@gmail.com>
2026-07-05 16:18:14 +0530

    Fixed: Swipe UP and Swipe Down Gesture issue

--- app/src/main/java/com/agupta07505/smartisland/ui/IslandOverlayView.kt
+++ app/src/main/java/com/agupta07505/smartisland/ui/IslandOverlayView.kt
@@ -72,6 +72,7 @@ fun IslandOverlayView(
     val currentOnToggle by rememberUpdatedState(onToggleExpanded)
     val currentOnDismiss by rememberUpdatedState(onDismissNotification)
     val currentOnOpenFloatingWindow by rememberUpdatedState(onOpenFloatingWindow)
+    val currentExpanded by rememberUpdatedState(expanded)
 
     val scope = rememberCoroutineScope()
     var dragOffset by remember { mutableStateOf(0f) }
@@ -210,9 +211,9 @@ fun IslandOverlayView(
                 }
                 .clip(RoundedCornerShape(radius))
                 .background(Color.Black)
-                .pointerInput(expanded) {
+                .pointerInput(Unit) {
                     detectTapGestures {
-                        if (expanded) {
+                        if (currentExpanded) {
                             SmartIslandOverlayService.resetTimer()
                         } else {
                             currentOnToggle()
@@ -227,7 +228,7 @@ fun IslandOverlayView(
                         },
                         onVerticalDrag = { change, dragAmount ->
                             dragAccumulator += dragAmount
-                            if (expanded) {
+                            if (currentExpanded) {
                                 change.consume()
                                 dragOffset = dragAccumulator.coerceIn(-100f * displayMetrics.density, 100f * displayMetrics.density)
                             }
@@ -235,7 +236,7 @@ fun IslandOverlayView(
                         onDragEnd = {
                             val swipeUpThreshold = -35f * displayMetrics.density
                             val swipeDownThreshold = 35f * displayMetrics.density
-                            if (expanded) {
+                            if (currentExpanded) {
                                 if (dragOffset < swipeUpThreshold) {
                                     currentOnDismiss()
                                 } else if (dragOffset > swipeDownThreshold) {
